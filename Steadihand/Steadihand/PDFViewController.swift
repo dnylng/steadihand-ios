c954f064bc824754ea4ce847a10683d6d8c37868
DannyLuong-Natgeo <danny.luong@natgeo.com>
2018-09-26 13:02:00 -0400

    Added device motion updates
    - Grabs user acceleration info
    - Double tap to reset view to the center
    - Tap 5 times to activiate prints of acceleration vector

--- Steadihand/Steadihand/PDFViewController.swift
+++ Steadihand/Steadihand/PDFViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 import PDFKit
+import CoreMotion
 
-class PDFViewController: UIViewController {
+class PDFViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet var scrollView: UIScrollView!
     
     var pdfView = PDFView()
     var pages = [UIImage]()
+    var motionManager = CMMotionManager()
     
     let pageWidth = UIScreen.main.bounds.width
     let pageHeight = UIScreen.main.bounds.height
+    
+    var printUpdates = false
+    
+    
+    // Acceleration calc variables
+    var time = 0.0
+    var acceleration = [Double](repeating: 0.0, count: 3)
+    var velocity = [Double](repeating: 0.0, count: 3)
+    var position = [Double](repeating: 0.0, count: 3)
+    
+    // MARK:- UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,10 +40,38 @@ class PDFViewController: UIViewController {
         scrollView.delegate = self
         scrollView.frame = UIScreen.main.bounds
         scrollView.contentSize.width = pageWidth * CGFloat(pages.count)
-    
+        
         populateScrollView()
+        positionResetTapGesture()
+        printUpdatesTapGesture()
+        
+        startDeviceMotionUpdates()
     }
- 
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        motionManager.stopAccelerometerUpdates()
+        super.viewDidDisappear(animated)
+    }
+    
+    // MARK:- Accelerometer updates
+    
+    func startDeviceMotionUpdates() {
+        if self.motionManager.isDeviceMotionAvailable, let queue = OperationQueue.current {
+            motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical, to: queue) { (data, error) in
+                if error != nil {
+                    NSLog("DEVICE MOTION ERROR: \(error?.localizedDescription ?? "Unable to read Device Motion updates")")
+                    return
+                }
+                
+                if let x = data?.userAcceleration.x, let y = data?.userAcceleration.y, let z = data?.userAcceleration.z {
+                    if self.printUpdates { print("DEVICE MOTION UPDATES: x: \(x), y: \(y), and z: \(z)") }
+                }
+            }
+        }
+    }
+    
+    // MARK:- Helper methods
+    
     func populateArray() {
         if let path = Bundle.main.path(forResource: "scottpilgrim", ofType: "pdf") {
             guard let pdfDocument = PDFDocument(url: URL(fileURLWithPath: path)) else { return }
@@ -57,8 +98,41 @@ class PDFViewController: UIViewController {
         }
     }
     
-}
-
-extension PDFViewController: UIScrollViewDelegate {
+    func translateView(x: CGFloat, y: CGFloat, z: CGFloat) {
+        scrollView.frame.origin.x -= x
+        scrollView.frame.origin.y += y
+    }
+    
+    // MARK:- UITapGestures
+    
+    func positionResetTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handlePositionReset(_:)))
+        tap.cancelsTouchesInView = false
+        tap.numberOfTapsRequired = 2
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handlePositionReset(_ recognizer: UITapGestureRecognizer) {
+        position = position.map { _ in 0.0 }
+        velocity = velocity.map { _ in 0.0 }
+        acceleration = acceleration.map { _ in 0.0 }
+        time = 0
+        
+        scrollView.frame.origin.x = 0
+        scrollView.frame.origin.y = 0
+        
+        print("Double tapped! Reset scroll view to the origin.")
+    }
+    
+    func printUpdatesTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handlePrintUpdates(_:)))
+        tap.cancelsTouchesInView = false
+        tap.numberOfTapsRequired = 5
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handlePrintUpdates(_ recognizer: UITapGestureRecognizer) {
+        self.printUpdates = !self.printUpdates
+    }
     
 }
---
+++ Steadihand/Steadihand/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Steadihand
+//
+//  Created by Danny Luong on 9/19/18.
+//  Copyright © 2018 dnylng. All rights reserved.
+//
+
+struct Constants {
+    static let NS2S = 1.0 / 1000000000.0
+    static let MAX_ACC = 5.0
+    static let MAX_POS_SHIFT = 100.0
+    static let MAX_ZOOM_FACTOR = 0.2
+}
