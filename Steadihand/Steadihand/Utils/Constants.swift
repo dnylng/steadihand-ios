b26a224254ca39b745773fed4235f8047e3ceb72
DannyLuong-Natgeo <danny.luong@natgeo.com>
2018-12-06 17:25:08 -0500

    Use NavigationController and add utils class

--- Steadihand/Steadihand/Utils/Constants.swift
+++ Steadihand/Steadihand/Utils/Constants.swift
@@ -8,9 +8,10 @@
 
 struct Constants {
     static let MAX_ACC = 5.0
-    static let MAX_POS_SHIFT = 100.0
+    static let MAX_POS_SHIFT = 30.0
     static let VELOCITY_FRICTION = 0.275
     static let POSITION_FRICTION = 0.45
     static let LOW_PASS_FILTER_ALPHA = 0.85
     static let VELOCITY_AMPLIFICATION  = 12650.0
+    static let GRAVITY = 9.81
 }
